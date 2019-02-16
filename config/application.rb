5b0d70d881436e9afb159e2228b7ed37983eea75
Luke Breen <l.breen1@outlook.com>
2019-02-16 16:53:29 +0200

    Add

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module MercianStationGym
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.serve_static_files = true
-    # config.assets.enabled = true
+    config.assets.enabled = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

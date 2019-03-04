4f4a6c7ea0d54dd45f58a96de73ecaedef58aae2
Luke Breen <l.breen1@outlook.com>
2019-03-04 19:19:47 +0200

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,17 +3,19 @@ User.destroy_all
 
 User.create(email: "l.b@gmail.com", password: "123456")
 
+description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
 # Competitions
 Competition.create(
   name: "Station Commander's Cup",
-  description: "INSERT DESCRIPTION",
+  description: description,
   image: "epi_station_logo.png",
   contain_image: true
 )
 
 Competition.create(
   name: "BFC Sporting Events",
-  description: "INSERT DESCRIPTION",
+  description: description,
   image: "bfc_crest.jpg",
   contain_image: true
 )
